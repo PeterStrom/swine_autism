099e08a87a2130ec40ec65bfabeda4a9153640fb
PeterStrom <peterstromprivat@gmail.com>
2018-03-06 20:58:35 +0100

    start joining data

--- raw_to_analysis.R
+++ raw_to_analysis.R
@@ -3,12 +3,10 @@
 # study.
 #
 # Registers:
-# 	*Cancer register 1958-2015
-# 	*Basal cell cancer register 2004-2015
+#   Medical birth register 1973-2012
 # 	Cause of death register 1952-2016
 # 	Outpatient register 1997-2016
 # 	Inpatient register 1964-2016
-# 	Prescription register (1 July 2005)-2016
 #
 # Cohort coverage:
 # 	The cohort covers 7 health care regions (61% of Swedens population). The
@@ -35,12 +33,15 @@ rm(wants, has)
 # #############################################################################
 # Read data
 # #############################################################################
-prescr <- haven::read_sas("../data/ut_lmed_10516_2017.sas7bdat")
 codreg <- haven::read_sas("../data/ut_dors_10516_2017.sas7bdat")
 outpat <- haven::read_sas("../data/ut_par_ov_10516_2017.sas7bdat")
 in_pat <- haven::read_sas("../data/ut_par_sv_10516_2017.sas7bdat")
 fg_inf <- haven::read_sas("../data/fg_info.sas7bdat")
 mf_reg <- haven::read_sas("../data/mfr.sas7bdat")
+
+moth_comorb_out <- read.csv("../data/oppenvard_comorb.csv")
+moth_comorb_in <- read.csv("../data/slutenvard_comorb2.csv")
+exc <- read.csv("../data/___.csv")
 
 # #############################################################################
 # Remove child ID with NA (still born) from medical birth register.
@@ -50,8 +51,42 @@ mf_reg = filter(mf_reg, !is.na('lpnr_BARN'))
 # #############################################################################
 # Join data.
 # #############################################################################
+# Keep track of excluded subjects.
+excluded <- data.frame(Reason = "All in FG", N = dim(fg_inf)[1], 
+                       stringsAsFactors = FALSE)
+# MFR
 dta <- inner_join(fg_inf, select(mf_reg, -lpnr_mor), by='lpnr_BARN')
-dta$age_at_vacc <- lubridate::ymd(dta$BFODDAT) - dta$vdat1
-any(is.na(lubridate::ymd(dta$BFODDAT)))
-summary(lubridate::ymd(dta$BFODDAT))
-           
+excluded <- rbind(excluded, list("Not in MFR", dim(dta)[1]))
+
+# Select subset of columns in cohort 
+dta <- subset(dta, select=c("lpnr_BARN", "lpnr_mor", "vdat1", "educ",
+                            "ethnew", "lan", "dispink", "excl_reason", 
+                            "BFODDAT", "KON", "MFODDAT", "GRDBS", 
+                            "ROK1", "MVIKT", "MLANGD", "PARITET",
+                            "INDATMHV"))
+
+# Dates
+dta$BFODDAT <- lubridate::ymd(dta$BFODDAT)
+dta$age_at_vacc <- dta$BFODDAT - dta$vdat1
+
+# restrict to 2001-01-01 to 2011-12-31 due to coverage in oppen/slutenvard.
+# (email 2015-09-15).
+dta <- subset(dta, BFODDAT >= "2001-01-01" & BFODDAT < "2012-01-01") 
+excluded <- rbind(excluded, list("Not in 2001_2011", dim(dta)[1]))
+
+# Not eligeble to participate 
+dta <- subset(dta, excl_reason == "Eligible", select = -excl_reason)
+excluded <- rbind(excluded, list("Not Eligable (FG)", dim(dta)[1]))
+
+# Exclude chromosome caused malformation (Jonas definies these criteria).
+dta <- dta[!(dta$lpnr_BARN %in% exc), ]
+excluded <- rbind(excluded, list("Chromosome (Jonas exc-file)", dim(dta)[1]))
+
+# Exclude missing pregnancy length GRDBS 
+dta <- dta[!is.na(dta$GRDBS), ]
+excluded <- rbind(excluded, list("Missing pregnancy days", dim(dta)[1]))
+
+# Exclude missing/unknown sex 
+dta <- dta[dta$KON %in% 1:2, ]
+excluded <- rbind(excluded, list("Missing/unknown sex", dim(dta)[1]))
+
